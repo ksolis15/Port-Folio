f120352c84c100aa6d8dea1628275ff46830e153
KevSol <71417421+ksolis15@users.noreply.github.com>
2023-06-09 00:45:41 -0600

    Adding components

---
+++ script.js
@@ -0,0 +1,4 @@
+const action = ['Studying', 'Swimming', 'Running', 'Hiking'];
+const ocurrance = ['Sometimes', 'Always', 'Mostly', 'Ocassionally'];
+const adjective = ['Boring', 'Tiring', 'Cool', 'Entertaining'];
+const days = ['Monday', 'Tuesday', 'Wedmesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
