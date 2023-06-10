c35d3b35fe40b0badddb1ff293fe4971f8ee859e
KevSol <71417421+ksolis15@users.noreply.github.com>
2023-06-09 23:06:06 -0600

    Selecting a random word for each instance

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const action = ['Studying', 'Swimming', 'Running', 'Hiking'];
-const ocurrance = ['Sometimes', 'Always', 'Mostly', 'Ocassionally'];
-const adjective = ['Boring', 'Tiring', 'Cool', 'Entertaining'];
-const days = ['Monday', 'Tuesday', 'Wedmesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const ocurrance = ['sometimes', 'always', 'mostly', 'ocassionally'];
+const adjective = ['boring', 'tiring', 'cool', 'entertaining'];
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const generator = () =>{
+    const randomAction = action[Math.floor(Math.random() * action.length)]; // Selecting a random action
+    const randomOcurrance = ocurrance[Math.floor(Math.random() * ocurrance.length)]; // Selecting a random ocurrance
+    const randomAdjective = adjective[Math.floor(Math.random() * adjective.length)]; // Selecting a random adjective
+    const randomdays = days[Math.floor(Math.random() * days.length)]; // Selecting a random day
+    
+};
